09aefe45420e1d5ae7d1d3d0001d9d4f9905ac47
liwei <505282051@qq.com>
2016-04-23 01:29:19 +0800

    2014-04-23 新建项目

---
+++ OCScanAndQR/OCScanAndQR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCScanAndQR
+//
+//  Created by 张星星 on 16/4/23.
+//  Copyright © 2016年 LW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCScanAndQR/OCScanAndQR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCScanAndQR
+//
+//  Created by 张星星 on 16/4/23.
+//  Copyright © 2016年 LW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
